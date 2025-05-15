70443cbf983e95753381a7c656b3b0a29af749e8
MrErm <capunkasavadikap@gmail.com>
2025-05-15 23:04:00 +0300

    функции find, split

--- First.py
+++ First.py
@@ -1,1 +1,16 @@
-print("Hello!")
+# S = "hello"
+# L = list(S)
+# print(L)
+# L[0] = "H"
+# print(L)
+
+# stringFromList = ''.join(L)
+# print(type(stringFromList))
+# print(stringFromList)
+
+str = "Привет"
+print(str.find('ве'))
+
+month = "январь, февраль, март, апрель"
+
+print(month.split(','))

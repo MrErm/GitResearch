1f0705c0fb3a45964c6e501e04633a5ae8e95b60
MrErm <capunkasavadikap@gmail.com>
2025-05-20 22:48:45 +0300

    методы классов

---
+++ Class.py
@@ -0,0 +1,24 @@
+# class Person:
+#     def __init__(self):
+#         print("Это человек")
+# tom = Person()        
+
+# class Man:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# nik = Man("Ник", 22)
+
+# print(nik.name)
+# print("ему",nik.age)
+# nik.company = "Yandex"
+# print(f"Работает в", nik.company)
+
+class Human():
+    def morning(self, hello):
+        print(hello)
+
+alex = Human()
+alex.morning("Доброе утро")
+

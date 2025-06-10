5fda6ed0035749e339694a2eb9d661b01943a815
MrErm <capunkasavadikap@gmail.com>
2025-06-10 17:24:33 +0300

    Добавлен просмотр информации о пользователе

--- MyBot/bot.py
+++ MyBot/bot.py
@@ -2,13 +2,13 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 async def hello_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("Вызван старт")
+    print("Вызван старт", update)
     await update.message.reply_text("Reality is overrated!")
 
 
 
 def main():
-    mybot = ApplicationBuilder().token("7892550183:AAG2ULPX_lTrU_zw6ivJjQU48FvQ1iVlWIM").build()
+    mybot = ApplicationBuilder().token("...............................").build()
 
    
     mybot.add_handler(CommandHandler("start", hello_user))

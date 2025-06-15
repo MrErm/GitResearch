2c91cd31241b8e14c629ec50d3f49b6dfde5d1bd
MrErm <capunkasavadikap@gmail.com>
2025-06-15 18:53:21 +0300

    Функция по продаже смартфонов с конструкцией if

--- Class.py
+++ Class.py
@@ -65,12 +65,42 @@ from pprint import pprint
 # product["recomended"].append("Ipad")
 # pprint(product)
 
-stock = [
-    {"name": "iPhone", "stock": 24, "price": 120000,
-     "recomended":["Iphone 16", "Samsung", "Xiaomi"]},
-    {"name": "iPad", "stock": 8, "price": 99000, "discount": 2300},
-    {"name": "Xiaomi", "stock": 41, "price": 35000.5,}
-]
-stock[0]["recomended"].append("Xiaomi Mi11")
-stock[2]["price"] = stock[2]["price"] - 20000
-pprint(stock)
+# stock = [
+#     {"name": "iPhone", "stock": 24, "price": 120000,
+#      "recomended":["Iphone 16", "Samsung", "Xiaomi"]},
+#     {"name": "iPad", "stock": 8, "price": 99000, "discount": 2300},
+#     {"name": "Xiaomi", "stock": 41, "price": 35000.5,}
+# ]
+# stock[0]["recomended"].append("Xiaomi Mi11")
+# stock[2]["price"] = stock[2]["price"] - 20000
+# pprint(stock)
+
+
+
+
+
+
+phone1 = {'name': 'iPhone 16 Pro', 'stock': 24, 'price': 65475.4, 'discount': 15}
+phone2 = {'name': 'Samsung Galaxy', 'stock': 8, 'price': 50000, 'discount': 10}
+phone3 = {'name': '', 'stock': 18, 'price': 80000, 'discount': 5}
+
+
+def discounted(price, discount, max_discount = 20, name=''):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    if max_discount > 99:
+        raise ValueError('Слишком большпая максимальная скидка')
+    if discount >= max_discount or "iphone" in name.lower() or not name:
+        return price
+    else:
+        return price - (price * discount / 100)
+    
+apple_desc = discounted(phone1["price"], phone1["discount"], name=phone1["name"])
+print(apple_desc)
+
+android_desc = discounted(phone2["price"], phone2["discount"], name=phone2["name"])
+print(android_desc)
+
+noname = discounted(phone3["price"], phone3["discount"], name=phone3["name"])
+print(noname)
